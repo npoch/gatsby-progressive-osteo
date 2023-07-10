96e3ffc41b38559ce5013cba6cf7c8d90246d061
npoch <nathanpoch@gmail.com>
2023-07-09 22:24:45 -0400

    fix CSP 2.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,8 +100,8 @@ module.exports = {
         "style-src": "'self' 'unsafe-inline' www.googletagmanager.com docs.google.com/forms fonts.googleapis.com *",
         "img-src": "'self' data: www.google-analytics.com www.googletagmanager.com docs.google.com/forms fonts.googleapis.com *",
         "script-src-elem": "www.googletagmanager.com/ *",
-        "connect-src": "'self' msr25ovq.api.sanity.io/v1/graphql/production/default *",
-        "style-src-elem": "fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap *"
+        "connect-src": "'self' msr25ovq.api.sanity.io/v1/graphql/production/default ",
+        // "style-src-elem": "fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap *"
         // you can add your directives or override defaults
       }
     }
