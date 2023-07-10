f18ff31fac2d149d8e7767c5d404d863b3469733
npoch <nathanpoch@gmail.com>
2023-07-09 22:42:01 -0400

    fix CSP 2.4

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,11 +95,12 @@ module.exports = {
       mergeStyleHashes: false, // you can disable styles sha256 hashes
       mergeDefaultDirectives: true,
       directives: {
-        "script-src": "'self' 'unsafe-inline' www.google-analytics.com www.googletagmanager.com docs.google.com/forms",
+        "script-src": "'self' 'unsafe-inline' 'wasm-unsafe-eval' 'inline-speculation-rules' www.google-analytics.com www.googletagmanager.com docs.google.com/forms",
         "style-src": "'self' 'unsafe-inline' www.googletagmanager.com docs.google.com/forms fonts.googleapis.com",
         "img-src": "'self' data: www.google-analytics.com www.googletagmanager.com docs.google.com/forms fonts.googleapis.com cdn.sanity.io 'unsafe-inline'",
-        "script-src-elem": "'unsafe-inline' www.googletagmanager.com/ ",
-        "connect-src": "'self' msr25ovq.api.sanity.io/v1/graphql/production/default ",
+        "script-src-elem": "'self' 'unsafe-inline' www.googletagmanager.com",
+        "connect-src": "'self' msr25ovq.api.sanity.io/v1/graphql/production/default www.google-analytics.com",
+        "media-src": "'self' 'unsafe-inline' data:"
         // "style-src-elem": "fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap *"
         // you can add your directives or override defaults
       }
