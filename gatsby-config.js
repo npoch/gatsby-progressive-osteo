78214eca87f4624aefb481582de6fef555805b9c
npoch <nathanpoch@gmail.com>
2023-07-09 22:21:15 -0400

    fix CSP 2.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,12 +96,12 @@ module.exports = {
       mergeStyleHashes: false, // you can disable styles sha256 hashes
       mergeDefaultDirectives: true,
       directives: {
-        "script-src": "'self' 'unsafe-inline' www.google-analytics.com www.googletagmanager.com docs.google.com/forms",
-        "style-src": "'self' 'unsafe-inline' www.googletagmanager.com docs.google.com/forms fonts.googleapis.com",
-        "img-src": "'self' data: www.google-analytics.com www.googletagmanager.com docs.google.com/forms fonts.googleapis.com",
-        "script-src-elem": "www.googletagmanager.com/",
-        "connect-src": "'self' msr25ovq.api.sanity.io/v1/graphql/production/default",
-        "style-src-elem": "fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+        "script-src": "'self' 'unsafe-inline' www.google-analytics.com www.googletagmanager.com docs.google.com/forms *",
+        "style-src": "'self' 'unsafe-inline' www.googletagmanager.com docs.google.com/forms fonts.googleapis.com *",
+        "img-src": "'self' data: www.google-analytics.com www.googletagmanager.com docs.google.com/forms fonts.googleapis.com *",
+        "script-src-elem": "www.googletagmanager.com/ *",
+        "connect-src": "'self' msr25ovq.api.sanity.io/v1/graphql/production/default *",
+        "style-src-elem": "fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap *"
         // you can add your directives or override defaults
       }
     }
